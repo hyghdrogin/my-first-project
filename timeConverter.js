f3cc599a6f1c869b6615bffa2d0c727c194e950b
Omopariola Emmanuel <93857121+AlagbaH@users.noreply.github.com>
2022-06-22 14:22:07 -0700

    Update timeConverter.js

--- timeConverter.js
+++ timeConverter.js
@@ -1,38 +1,27 @@
-const inputValue = document.getElementById('abc');
-console.log('first', inputValue)
-inputValue.addEventListener('oninput', () => {
-    console.log(inputValue.value);
-})
+const inputValue = document.getElementById('abc').value;
+const submitBtn = document.getElementById('submit');
+let twelveHours;
 
-// let tInput = document.getElementById('abc');
-const submitBtn = document.getElementById('submit')
-// //Calculate the time conversion
-// let twelveHours;
-// // tInput.addEventListener ('input', () => {
-// //     twelveHours = parseInt(tInput, 10)
-// // })
-// let emma = tInput.value
-// let time;
-submitBtn.addEventListener('click', () => {
-    //ediTime(tInput);
-    console.log('dada');
-    console.log('aaa', inputValue);
+inputValue.addEventListener ('input', () => {
+    twelveHours = parseInt(tInput, 10)
 });
-// twelveHours = parseInt(tInput, 10)
-// function ediTime(tInput) {
+
+let time;
+
+twelveHours = parseInt(inputValue, 10)
+function ediTime(inputValue) {
+    time = tInput.split(":");
+    const hour = parseInt(time[0]);
+    const minute = parseInt(time[1]);
     
-//     time = tInput.split(":");
-//     const hour = parseInt(time[0]);
-//     const minute = parseInt(time[1]);
-//     console.log(tInput);
+    if ( hour >= 13) {
+        return `The time is ${hour} : ${minute} pm.`;
+    }
+    if (hour <= 12) {
+        return `The time is ${hour} : ${minute} am.`;
+    }
+}
 
-//     console.log(time);
-//     if ( hour >= 13) {
-//     return `The time is ${hour} : ${minute} pm.`;
-//     }
-
-//     if (hour <= 12) {
-//     return `The time is ${hour} : ${minute} am.`;
-//     }
-// }
-
+submitBtn.addEventListener('click', () => {
+    ediTime(inputValue);
+});
